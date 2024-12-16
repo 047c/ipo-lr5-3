4ab7bba49405fcb891d7e7f6040d0fb913e7ddd6
047c <maks.smirnov.smolensk@gmail.com>
2024-12-16 19:43:29 +0300

    renaming

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 with open("text.txt", 'r') as file:  # Открываем файл, выдав имя file
-    stroka = file.read()  # Получаем строку
-slova = stroka.split()  # Разделяем ее на слова, записав в список
-print(f'Количество слов в файле "text.txt" {len(slova)}')  # Выдаем сообщение с количеством слов
+    stringg = file.read()  # Получаем строку
+words = stringg.split()  # Разделяем ее на слова, записав в список
+print(f'Количество слов в файле "text.txt" {len(words)}')  # Выдаем сообщение с количеством слов
